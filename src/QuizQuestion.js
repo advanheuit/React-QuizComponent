e07a3d0083a087934284d82a7a5e65403d4e0f32
Van Heuit <Andy.Vanheuit@Wildlife.ca.gov>
2019-01-08 14:44:49 -0800

    handle incorrect answers.

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { incorrectAnswer: false }
+  }
   handleClick(button_text) {
     if(button_text === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler()
+      this.setState({ incorrectAnswer: false })
+    } else {
+      this.setState({ incorrectAnswer: true })
     }
   }
   render() {
@@ -20,6 +27,7 @@ class QuizQuestion extends Component {
            })}
          </ul>
        </section>
+       {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
      </main>
     )
   }
